775f1ea1998f8547ed249a1f18c4ee3feac6e823
tylerjohnbecker <tylerjohnbecker@gmail.com>
2024-07-14 15:12:31 -0700

    removed a bunch of annoying files before pushing

--- knowledge_server/install/knowledge_server/share/knowledge_server/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(knowledge_server_IDL_FILES "msg/Pair.idl;msg/Update.idl;srv/Fetch.idl")
-set(knowledge_server_INTERFACE_FILES "msg/Pair.msg;msg/Update.msg;srv/Fetch.srv;srv/Fetch_Request.msg;srv/Fetch_Response.msg")
